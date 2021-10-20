0cc118f6e36f471a27213861a9870564d7004e92
spoturno <spoturno16@gmail.com>
2021-10-20 09:47:05 -0300

    kph convertion

--- anemometro/anemometro.ino
+++ anemometro/anemometro.ino
@@ -1,6 +1,6 @@
 const int ENC_COUNT_REV = 600;
 const int ENC_IN = 2;
-
+const int radious = 15; // radio de anemometro en metros
 
 volatile long pulse;
 int interval = 1000; //intervalo de 1s para medidas
@@ -10,10 +10,11 @@ long previousMillis = 0;
 long currentMillis = 0;
 
 int rpm; // rev per min
+int kph; // kilom per h
 
 void setup() {
   Serial.begin(9600);
-  pinMode(ENC_IN, INPUT);
+  pinMode(ENC_IN, INPUT_PULLUP);
   //pinMode(encoderPinB, INPUT);
   attachInterrupt(digitalPinToInterrupt(ENC_IN), updateEncoder, RISING);
   previousMillis = millis();
@@ -30,19 +31,27 @@ void loop() {
   //update rpm value every second
   currentMillis = millis();
   if(currentMillis - previousMillis > interval){ // cuantos pulsos ocurren en intervalo de 1s
+    detachInterrupt(digitalPinToInterrupt(ENC_IN));
     previousMillis = currentMillis;
     //multiplicando por 60 obtenemos la cantidad en 1 minuto
     //dividimos por el total de pulsos x rev del encoder 
     rpm = pulse * 60 / ENC_COUNT_REV; 
-    
-    Serial.print("PULSES:");
-    Serial.print(pulse);
-    Serial.print('\n');
-    Serial.print("SPEED:");
-    Serial.print(rpm);
-    Serial.print("RPM");
-    Serial.print('\t');
+    kph = (0.1885 * rpm * 2 * radious) / 100;
+
+    if(rpm > 0){
+      Serial.print("PULSES:");
+      Serial.print(pulse);
+      Serial.print('\n');
+      Serial.print("SPEED:");
+      Serial.print(rpm);
+      Serial.print("RPM");
+      Serial.print('\t');
+      Serial.print("KPH:");
+      Serial.print(kph);
+      Serial.print('\t');
+    }
     pulse = 0;
+    attachInterrupt(digitalPinToInterrupt(ENC_IN), updateEncoder, RISING);
   
   }
   
